b7a1b840c96f2b263d4468d02f2847181a85cbf4
Eduardo Rabelo <oieduardorabelo@gmail.com>
2016-12-09 22:43:57 +1300

    updating config

--- jest.context.js
+++ jest.context.js
@@ -1,8 +1,8 @@
 const glob = require('glob');
 
-require('./app/bower_components/angular/angular.js');
-require('./app/bower_components/angular-route/angular-route.js');
-require('./app/bower_components/angular-mocks/angular-mocks.js');
+require('angular/angular.js');
+require('angular-route/angular-route.js');
+require('angular-mocks/angular-mocks.js');
 
 glob.sync('./app/components/**/*.js').forEach(file => require(file));
 glob.sync('./app/view*/**/*.js').forEach(file => require(file));
