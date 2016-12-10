8475b3cf3a8132a9b375ede38533dd6bf61b79f5
Eduardo Rabelo <oieduardorabelo@gmail.com>
2016-12-10 23:32:14 +1300

    updating jest setup

--- jest.context.js
+++ jest.context.js
@@ -1,8 +1,19 @@
+//
+// angular-mocks on line 2731, try to find jasmineOrMocha
+// in the window object, so, we need to load this asset here,
+// because, in this moment, Jest has installed the test framework environment
+//
+// also, we can't use just "module", to mock the angular modules,
+// we need to explicit point to "window.module", where angular-mocks
+// will publish the mock object
+//
+require('angular/angular')
+require('angular-route/angular-route')
+require('angular-mocks/angular-mocks');
+
+//
+// Jest doesn't support glob in the "setupFiles" key
+//
 const glob = require('glob');
-
-require('angular/angular.js');
-require('angular-route/angular-route.js');
-require('angular-mocks/angular-mocks.js');
-
 glob.sync('./app/components/**/*.js').forEach(file => require(file));
 glob.sync('./app/view*/**/*.js').forEach(file => require(file));
